6227a7c3bc75db5fd8bd687a28272543138f39dc
falmufqi <falmufqi@gmail.com>
2023-05-24 19:05:44 +0700

    Projek Sekolah Updatean

--- kartu_daftar.php
+++ kartu_daftar.php
@@ -18,6 +18,8 @@ $result   = mysqli_fetch_array($query);
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="css/kartu.css" type="text/css" />
     <link rel="stylesheet" href="css/print.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
 
     <!-- icon web addres
     ============================================= -->
@@ -30,7 +32,59 @@ $result   = mysqli_fetch_array($query);
 </head>
 
 <body>
-    <div class="card" id="kartu">
+    <!-- Header Card-->
+    <header id="header" class="site-header clearfix no-print">
+        <div class="container">
+            <div class="row">
+
+                <!-- LOGO START
+                        ============================================= -->
+                <div class="logo col-md-2">
+                    <a href="index.php">
+                        <img src="image/logo_tk_cut.png" alt="" width="115px" height="115px">
+                        <!--<H3>SMK Taruna Karya 1</H3>-->
+                    </a>
+                </div>
+                <!-- LOGO END -->
+
+                <!-- NAVIGATION START
+                        ============================================= -->
+                <div class=" navigation col-md-10 text-right">
+
+                    <!-- MAIN NAVIGATION START
+                            ============================================= -->
+                    <nav id="main-menu" class="menu">
+                        <ul id="menu-top-menu" class="menus">
+                            <!--<li class="active"><a href="index.php">Home</a></li>-->
+                            <li><a href="index.php">Home</a></li>
+                            <!--<li><a href="profil.php">Profil Sekolah</a></li>-->
+                            <!--<li><a href="galeri.php">Gallery</a></li>-->
+                            <li class="has-child">
+                                <a href="#">Profile Sekolah</a>
+                                <ul class="child">
+                                    <li><a href="profil.php">Tentang Sekolah</a></li>
+                                    <li><a href="galeri.php">Gallery</a></li>
+                                    <li><a href="prestasi.php">Prestasi</a></li>
+                                    <li><a href="guru.php">Data Guru</a></li>
+                                </ul>
+                            <li><a href="agenda.php">Agenda</a></li>
+                            <li class="has-child">
+                                <a href="#">Informasi</a>
+                                <ul class="child">
+                                    <li><a href="info.php">Informasi Pendaftaran</a></li>
+                                    <li><a href="data_pendaftar.php">Data Pendaftar</a></li>
+                                </ul>
+                            <li><a class="btn" style="background-color:#ffb703" onclick="JavaScript:window.location.href='kartu_daftar.php?file=kartu peserta.pdf';">Cetak Kartu</a></li>
+                        </ul>
+                    </nav>
+                    <!-- MAIN NAVIGATION END -->
+                </div>
+                <!-- NAVIGATION END -->
+            </div>
+        </div>
+    </header>
+
+    <div id="kartu" class="center">
         <img src="image/logo_tk_hd.png" alt="Taruna Karya 1 Karawang" style="display: block; margin-top: 10px; margin-left: 5cm;" height="150px" width="150px">
         <h1 align="center">SMK TARUNA KARYA 1</h1>
         <h2 align="center">Kartu Pendaftaran</h2>
@@ -75,38 +129,35 @@ $result   = mysqli_fetch_array($query);
         <div style="width: 50%; text-align: left; float: right;">Karawang, <?= date('d-M-Y'); ?></div><br>
     </div>
 
-    <br>
-    <div>
-        <button class="no-print block" onclick="JavaScript:window.location.href='kartu_daftar.php?file=kartu peserta.pdf';"> Download</button><br />
-    </div>
     <!-- FOOTER START
             ============================================= -->
 
-    <!--<footer id="footer">
-        <div class="container">
+    <footer id="footer">
+        <div class="container no-print">
             <div class="row">
 
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8 pull-left wow fadeInUp">
-                            <h3>Silahkan klik tombol download kartu, untuk mendownload kartu pendaftaran !</h3>
+                            <h3>Silahkan klik tombol cetak kartu, untuk mencetak kartu pendaftaran !</h3>
                         </div>
 
                         <div class="col-md-4 wow fadeInUp">
                             <div class="button-normal white pull-right">
-                                <a href="pendaftaran.php" class="no-margin">Download Kartu</a>
+                                <a class="no-margin no-print" onclick="JavaScript:window.location.href='kartu_daftar.php?file=kartu peserta.pdf';">Cetak Kartu</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>-->
+    </footer>
 
     <!-- FOOTER END -->
     <script>
         window.print();
     </script>
 </body>
+
 
 </html>
